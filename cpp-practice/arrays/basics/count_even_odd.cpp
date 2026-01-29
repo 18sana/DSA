497e8d4e1c1378021697d93169140fef731758f1
Sana Asiwal <sanaasiwal18@gmail.com>
2026-01-29 11:17:10 +0530

    Implement functionality to count even and odd numbers in an array and reverse an array in count_even_odd.cpp and reverse_array.cpp

--- cpp-practice/arrays/basics/count_even_odd.cpp
+++ cpp-practice/arrays/basics/count_even_odd.cpp
@@ -1,7 +1,22 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-
-int main() {
-    // TODO: Implement
-    return 0;
+int main(){
+  int n;
+  cin>>n;
+  int arr[n];
+  for(int i=0;i<n;i++){
+    cin>>arr[i];
+  }
+  int count_even=0;
+  int count_odd=0;
+  for(int i=0;i<n;i++){
+    if(arr[i]%2==0){
+      count_even++;
+    }else{
+      count_odd++;
+    }
+  }
+  cout<<"Even : "<< count_even<<endl;
+  cout<<"Odd : "<< count_odd<<endl;
+  return 0;
 }
