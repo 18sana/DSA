2290c3abfc5aee8178261b525609f68f63d9d585
Sana Asiwal <sanaasiwal18@gmail.com>
2026-02-26 15:55:10 +0530

    Implement functionality to find the second largest element, perform linear search, and count positive, negative, and zero values in arrays in second_largest.cpp, linear_search.cpp, and positive_negative.cpp respectively.

--- cpp-practice/arrays/manipulation/second_largest.cpp
+++ cpp-practice/arrays/manipulation/second_largest.cpp
@@ -1,7 +1,24 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-
-int main() {
-    // TODO: Implement
-    return 0;
+int main(){
+  int n;
+  cin>>n;
+  int arr[n];
+  
+  for(int i =0;i<n;i++){
+    cin>>arr[i];
+  }
+  int largest=arr[0];
+  int second_largest = -1;
+  for(int i=0;i<n;i++){
+    if(arr[i]>largest){
+      second_largest =largest;
+      largest = arr[i];
+    }
+    else if(arr[i]>second_largest && arr[i] != largest){
+      second_largest=arr[i];
+    }
+  }
+  cout<<"second_largest: "<<second_largest<<endl;
+  return 0;
 }
--- cpp-practice/basics/conditions/positive_negative.cpp
+++ cpp-practice/basics/conditions/positive_negative.cpp
@@ -1,7 +1,31 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-
-int main() {
-    // TODO: Implement
-    return 0;
+int main(){
+  int n;
+  cin>>n;
+  int arr[n];
+  for(int i=0;i<n;i++){
+    cin>>arr[i];
+  }
+  int positive = 0;
+  int negative = 0;
+  int zero = 0;
+  
+  for(int i=0;i<n;i++){
+    if(arr[i]>0){
+      positive++;
+    }
+    else if(arr[i]<0){
+      negative++;
+    }
+    else{
+      zero++;
+    }
+    
+    
+  }
+  cout<<"Positive = "<<positive<<endl;
+  cout<<"Negative = "<<negative<<endl;
+  cout<<"Zero = "<<zero<<endl;
+  return 0;
 }
