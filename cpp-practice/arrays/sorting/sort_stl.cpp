f91d1e76553e949ea7c268c0f583ab3844502181
Sana Asiwal <sanaasiwal18@gmail.com>
2026-04-27 11:24:52 +0530

    Refactor reverse_array.cpp to use a swapping method for reversing the array and improve output formatting. Update sort_stl.cpp to check if the array is sorted and provide appropriate output. Clean up reverse_number.cpp for consistent formatting.

--- cpp-practice/arrays/sorting/sort_stl.cpp
+++ cpp-practice/arrays/sorting/sort_stl.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    // TODO: Implement
+int main() 
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    bool sorted = true;
+    for(int i =0;i<n;i++){
+      cin>>arr[i];
+    }
+    for(int i =0;i<n-1;i++){
+      if(arr[i]>arr[i+1]){
+        sorted = false;
+        break;
+      }
+      else{
+        sorted = true;
+      }
+     
+    }
+     if(sorted){
+        cout<<"Sorted";
+        
+      }else{
+        cout<<"Not Sorted";
+      }
     return 0;
 }
