66789595baa6308cc663518077cc58f5801ba58f
Sana Asiwal <sanaasiwal18@gmail.com>
2026-05-12 16:38:51 +0530

    Implement functionality to find the largest element in an array in largest_element.cpp

--- cpp-practice/arrays/basics/largest_element.cpp
+++ cpp-practice/arrays/basics/largest_element.cpp
@@ -1,7 +1,16 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-
-int main() {
-    // TODO: Implement
-    return 0;
+int main(){
+  int n;
+  cin>>n;
+  int arr[n];
+  int largest=arr[0];
+  for(int i =0;i<n;i++){
+    cin>>arr[i];
+    if(arr[i]>largest){
+        largest=arr[i];
+    }
+  }
+  cout<<largest;
+  return 0;
 }
